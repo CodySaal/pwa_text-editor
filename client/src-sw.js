0712ae34148c1011ee9c7bbb7718c0b2713642a9
Cody <110630101+CodySaal@users.noreply.github.com>
2023-01-23 22:26:37 -0600

    previous note generated on refresh

--- client/src-sw.js
+++ client/src-sw.js
@@ -24,17 +24,17 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+// registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute(
-  ({ request }) => ["style", "script", "worker"].includes(request.destination),
-  new StaleWhileRevalidate({
-    cacheName: "asset-cache",
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-    ],
-  })
-);
+// // TODO: Implement asset caching
+// registerRoute(
+//   ({ request }) => ["style", "script", "worker"].includes(request.destination),
+//   new StaleWhileRevalidate({
+//     cacheName: "asset-cache",
+//     plugins: [
+//       new CacheableResponsePlugin({
+//         statuses: [0, 200],
+//       }),
+//     ],
+//   })
+// );
